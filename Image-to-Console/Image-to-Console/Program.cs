3f7feecfea5bb0d8c3c01ea843fa04220d98abe8
Peter Honzejk <peter.honzejk@outlook.com>
2023-06-01 13:44:02 +0200

    1.1.0

--- Image-to-Console/Image-to-Console/Program.cs
+++ Image-to-Console/Image-to-Console/Program.cs
@@ -11,25 +11,27 @@ namespace Image_to_Console
 
             while (!exitProgram)
             {
-                Console.WriteLine("\nZadejte cestu k souboru s obrázkem ve formátu .bmp:");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("\nZadejte cestu k obrázku ve formátu .bmp:");
+                Console.ResetColor();
+
                 string filePath = Console.ReadLine();
 
-                // Opakujte žádost o cestu k souboru, dokud není zadán platný vstup
-                while (!File.Exists(filePath) || Path.GetExtension(filePath)?.ToLower() != ".bmp")
+                // Ověření platnosti zadané cesty k souboru
+                while (string.IsNullOrWhiteSpace(filePath) || !File.Exists(filePath) || Path.GetExtension(filePath)?.ToLower() != ".bmp")
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("\nNeplatný vstup nebo nepodporovaný formát obrázku. Podporován je pouze formát .bmp. Prosím, zkuste to znovu:");
+                    Console.WriteLine("\nNeplatný vstup nebo nepodporovaný formát obrázku. Zkuste to znovu:");
                     Console.ResetColor();
                     filePath = Console.ReadLine();
                 }
 
-                // Načtěte soubor jako binární pole
-                byte[] fileBytes = File.ReadAllBytes(filePath);
+                byte[] fileBytes = ReadImageFile(filePath);
 
                 // Získání informací o obrázku z hlavičky souboru
-                int imageOffset = BitConverter.ToInt32(fileBytes, 0x0A);
-                int imageWidth = BitConverter.ToInt32(fileBytes, 0x12);
-                int imageHeight = BitConverter.ToInt32(fileBytes, 0x16);
+                int imageOffset = BitConverter.ToInt32(fileBytes, FileHeaderOffsets.ImageOffset);
+                int imageWidth = BitConverter.ToInt32(fileBytes, FileHeaderOffsets.ImageWidth);
+                int imageHeight = BitConverter.ToInt32(fileBytes, FileHeaderOffsets.ImageHeight);
 
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("\nObrázek úspěšně načten!");
@@ -40,45 +42,26 @@ namespace Image_to_Console
                 Console.WriteLine("Šířka: " + imageWidth);
                 Console.WriteLine("Výška: " + imageHeight);
 
-                // Výpočet poměru změny velikosti na základě rozměrů konzole
                 int consoleWidth = Console.WindowWidth;
                 int consoleHeight = Console.WindowHeight - 1;
-                double widthRatio = (double)imageWidth / consoleWidth;
-                double heightRatio = (double)imageHeight / consoleHeight;
-                double resizeRatio = Math.Max(widthRatio, heightRatio);
 
-                // Výpočet rozměrů zmenšeného obrázku
+                // Výpočet poměru změny velikosti na základě rozměrů konzole
+                double resizeRatio = CalculateResizeRatio(imageWidth, imageHeight, consoleWidth, consoleHeight);
+
                 int resizedWidth = (int)(imageWidth / resizeRatio);
                 int resizedHeight = (int)(imageHeight / resizeRatio);
 
-                // Vytvoření bufferu pro pixely zmenšeného obrázku
-                byte[] resizedImageBuffer = new byte[resizedWidth * resizedHeight];
-
-                // Zmenšení obrázku vzorkováním a kopírováním pixelů
-                for (int y = 0; y < resizedHeight; y++)
-                {
-                    for (int x = 0; x < resizedWidth; x++)
-                    {
-                        // Výpočet původní pozice pixelu v obrázku
-                        int originalX = (int)(x * resizeRatio);
-                        int originalY = (int)(y * resizeRatio);
-
-                        // Získání hodnoty pixelu z původního obrázku
-                        byte pixelValue = fileBytes[imageOffset + originalY * imageWidth + originalX];
-
-                        // Uložení hodnoty pixelu do zmenšeného bufferu
-                        resizedImageBuffer[y * resizedWidth + x] = pixelValue;
-                    }
-                }
+                byte[] resizedImageBuffer = ResizeImage(fileBytes, imageOffset, imageWidth, imageHeight, resizedWidth, resizedHeight, resizeRatio);
 
                 Console.WriteLine("Rozměry zmenšeného obrázku:");
                 Console.WriteLine("Šířka: " + resizedWidth);
                 Console.WriteLine("Výška: " + resizedHeight + "\n");
 
-                // Zobrazení zmenšeného obrázku v konzoli
                 RenderImage(resizedImageBuffer, resizedWidth, resizedHeight);
 
+                Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine("\nPřejete si převést další obrázek? (y/n)");
+                Console.ResetColor();
                 string choice = Console.ReadLine();
 
                 if (choice?.ToLower() != "y")
@@ -88,42 +71,89 @@ namespace Image_to_Console
             }
         }
 
+        static byte[] ReadImageFile(string filePath)
+        {
+            byte[] fileBytes;
+
+            try
+            {
+                fileBytes = File.ReadAllBytes(filePath);
+            }
+            catch (IOException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nChyba při čtení souboru: " + e.Message);
+                Console.ResetColor();
+                fileBytes = new byte[0];
+            }
+
+            return fileBytes;
+        }
+
+        static double CalculateResizeRatio(int imageWidth, int imageHeight, int consoleWidth, int consoleHeight)
+        {
+            double widthRatio = (double)imageWidth / consoleWidth;
+            double heightRatio = (double)imageHeight / consoleHeight;
+
+            return Math.Max(widthRatio, heightRatio);
+        }
+
+        static byte[] ResizeImage(byte[] fileBytes, int imageOffset, int imageWidth, int imageHeight, int resizedWidth, int resizedHeight, double resizeRatio)
+        {
+            byte[] resizedImageBuffer = new byte[resizedWidth * resizedHeight];
+
+            for (int y = 0; y < resizedHeight; y++)
+            {
+                for (int x = 0; x < resizedWidth; x++)
+                {
+                    int originalX = (int)(x * resizeRatio);
+                    int originalY = (int)(y * resizeRatio);
+
+                    byte pixelValue = fileBytes[imageOffset + originalY * imageWidth + originalX];
+
+                    resizedImageBuffer[y * resizedWidth + x] = pixelValue;
+                }
+            }
+
+            return resizedImageBuffer;
+        }
+
         static void RenderImage(byte[] imageBuffer, int imageWidth, int imageHeight)
         {
             int consoleWidth = Console.WindowWidth;
             int consoleHeight = Console.WindowHeight - 1;
 
-            // Výpočet úpravy poměru stran pro znaky
             double aspectRatio = (double)consoleWidth / consoleHeight;
-            double charAspectRatio = 2.0; // Upravte tuto hodnotu podle použitého písma
+            double charAspectRatio = 2.0;
 
-            // Výpočet poměrů šířky a výšky pro zobrazení obrázku v konzoli
             double widthRatio = (double)consoleWidth / imageWidth;
             double heightRatio = (double)consoleHeight / imageHeight;
 
-            // Vyberte poměr změny velikosti na základě úpravy poměru stran
             double resizeRatio = Math.Min(widthRatio / charAspectRatio, heightRatio);
 
-            // Výpočet rozměrů zmenšeného obrázku
             int resizedWidth = (int)(imageWidth * resizeRatio);
             int resizedHeight = (int)(imageHeight * resizeRatio);
 
-            // Projděte pixely a vykreslete je v konzoli
             for (int y = 0; y < resizedHeight; y++)
             {
                 for (int x = 0; x < resizedWidth; x++)
                 {
-                    // Získání hodnoty pixelu z bufferu
                     byte pixelValue = imageBuffer[(int)(y / resizeRatio) * imageWidth + (int)(x / resizeRatio)];
 
-                    // Nastavení barvy pixelu na základě hodnoty pixelu
                     char pixelColor = pixelValue == 0 ? '#' : ' ';
 
-                    // Výstup barvy pixelu do konzole
                     Console.Write(pixelColor);
                 }
                 Console.WriteLine();
             }
+        }
+
+        // Konstanty pro ofsety hlavičky souboru
+        static class FileHeaderOffsets
+        {
+            public const int ImageOffset = 0x0A;
+            public const int ImageWidth = 0x12;
+            public const int ImageHeight = 0x16;
         }
     }
 }
